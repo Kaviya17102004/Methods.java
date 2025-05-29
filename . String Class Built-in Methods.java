de65809bb7ea21892fc5d5c03d4d65cac78713a9
Kaviya K <kaviyapythondeveloper@gmail.com>
2025-05-29 21:37:05 +0530

    Create . String Class Built-in Methods.java

---
+++ . String Class Built-in Methods.java
@@ -0,0 +1,9 @@
+public class StringMethodsExample {
+    public static void main(String[] args) {
+        String name = "Kaviya";
+        System.out.println("Length: " + name.length());
+        System.out.println("Uppercase: " + name.toUpperCase());
+        System.out.println("Character at index 2: " + name.charAt(2));
+        System.out.println("Substring (1 to 4): " + name.substring(1, 4));
+    }
+}
