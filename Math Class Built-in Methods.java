7d3ed3b0a28ccc2a9d6cb22a57d7895cc2e7bae8
Kaviya K <kaviyapythondeveloper@gmail.com>
2025-05-29 21:38:37 +0530

    Create Math Class Built-in Methods.java

---
+++ Math Class Built-in Methods.java
@@ -0,0 +1,8 @@
+public class MathMethodsExample {
+    public static void main(String[] args) {
+        System.out.println("Square root of 16: " + Math.sqrt(16));
+        System.out.println("2 raised to 3: " + Math.pow(2, 3));
+        System.out.println("Max of 10 and 20: " + Math.max(10, 20));
+        System.out.println("Random number: " + Math.random());
+    }
+}
