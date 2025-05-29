40b87df787d9417ff883cd649101beedf2ffc44a
Kaviya K <kaviyapythondeveloper@gmail.com>
2025-05-29 21:43:24 +0530

    Create  System Class Built-in Method.java

---
+++ System Class Built-in Method.java
@@ -0,0 +1,7 @@
+public class SystemMethodExample {
+    public static void main(String[] args) {
+        System.out.println("This is a built-in method in System class.");
+        long time = System.currentTimeMillis();
+        System.out.println("Current Time in Milliseconds: " + time);
+    }
+}
